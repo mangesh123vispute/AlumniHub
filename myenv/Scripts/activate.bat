87c045d7b4263d3ea647ab6a1b3d863d1ed2223b
mangesh vispute <mangesh2003vispute@gmail.com>
2024-03-23 22:40:06 +0530

    Login and logout pages are improved

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\laxma\OneDrive\Desktop\dsa and practice\learning\django\Minor project\ALUMNI-TRACKING-SYSTEM\ALUMNI-TRACKING-SYSTEM\myenv
+set VIRTUAL_ENV=C:\Users\laxma\OneDrive\Desktop\coding\learning\django\Minor project\ALUMNI-TRACKING-SYSTEM\ALUMNI-TRACKING-SYSTEM\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
