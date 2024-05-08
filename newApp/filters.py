09dbb3e6695b9842e6e2073d450687381aec2e16
mangesh vispute <mangesh2003vispute@gmail.com>
2024-05-08 17:53:55 +0530

    Preparing for the New update

--- newApp/filters.py
+++ newApp/filters.py
@@ -5,9 +5,12 @@ import django_filters
 class AlumniFilter(django_filters.FilterSet):
     first_name = django_filters.CharFilter(lookup_expr='icontains')
     last_name = django_filters.CharFilter(lookup_expr='icontains')
-    College = django_filters.CharFilter(lookup_expr='icontains')
-    Year_Joined = django_filters.CharFilter(lookup_expr='icontains')
+    Branch = django_filters.CharFilter(lookup_expr='icontains')
+    skills = django_filters.CharFilter(lookup_expr='icontains')
+    About = django_filters.CharFilter(lookup_expr='icontains')
+    Work = django_filters.CharFilter(lookup_expr='icontains')
+
 
     class Meta:
         model = User
-        fields = ['College', 'Year_Joined']
+        fields = [ 'skills','Branch','Work','first_name', 'last_name','About',   ]
