cd851773639edf56037d484a2c798daa228ebe9c
mangesh vispute <mangesh2003vispute@gmail.com>
2024-09-17 15:00:45 +0530

    Templates added

--- Frontend/src/components/LandingPage.js
+++ Frontend/src/components/LandingPage.js
@@ -13,7 +13,7 @@ function LandingPage() {
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
-          alignItems: "center"
+          alignItems: "center",
         }}
       >
         <div
@@ -27,40 +27,87 @@ function LandingPage() {
             backgroundImage: `url(${home_background})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
-            filter: "brightness(20%)" // Adjust the brightness value as needed
+            filter: "brightness(20%)", // Adjust the brightness value as needed
           }}
         ></div>
-        
+
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title> SSBT COET</title>
-        <div style={{ position: "absolute", top: "70px", left: "200px", fontSize: "2em" }}>
-          <h1>SSBT COET</h1>
-        </div>
+        <title>
+          {" "}
+          Alumni Hub{" "}
+        </title>
+        {/* <div
+          style={{
+            position: "absolute",
+            top: "70px",
+            left: "200px",
+            fontSize: "2em",
+          }}
+        >
+          <h1>
+            Alumni <span style={{ color: "red" }}>Hub</span>{" "}
+          </h1>
+        </div> */}
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
           crossOrigin="anonymous"
         />
-        <div style={{ position: "absolute", top: "50px", right: "100px", fontSize: "1.5em" }}>
-          <NavLink to="/" className="text-white me-3 p-2 font-weight-bold" activeClassName="underline">Home</NavLink>
-          <NavLink to="/login" className="text-white me-3 p-2 font-weight-bold" activeClassName="underline">Login</NavLink>
-          <NavLink to="/register" className="text-white p-2 font-weight-bold" activeClassName="underline">Register</NavLink>
-        </div>
+        {/* <div
+          style={{
+            position: "absolute",
+            top: "50px",
+            right: "100px",
+            fontSize: "1.5em",
+          }}
+        >
+          <NavLink
+            to="/"
+            className="text-white me-3 p-2 font-weight-bold"
+            activeClassName="underline"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/login"
+            className="text-white me-3 p-2 font-weight-bold"
+            activeClassName="underline"
+          >
+            Login
+          </NavLink>
+          <NavLink
+            to="/register"
+            className="text-white p-2 font-weight-bold"
+            activeClassName="underline"
+          >
+            Register
+          </NavLink>
+        </div> */}
 
         <header>
           <div className="header-content" style={{ fontSize: "1.2em" }}>
-            <h1>SSBT COET</h1>
-            <p>Lets Get Ready For Exam</p>
+            <h1>
+              Alumni <span style={{ color: "red" }}>Hub</span>{" "}
+            </h1>
+            <p>Lets Connect ,Lets Grow</p>
             <div style={{ display: "flex", justifyContent: "space-evenly" }}>
               <NavLink to="/login">
-                <button type="button" className="btn btn-light" style={{ fontSize: "1.5em" }}>
+                <button
+                  type="button"
+                  className="btn btn-light"
+                  style={{ fontSize: "1.5em",borderRadius: "10px", border: "1px solid  " }}
+                >
                   LogIn
                 </button>
               </NavLink>
               <NavLink to="/register">
-                <button type="button" className="btn btn-light" style={{ fontSize: "1.5em" }}>
+                <button
+                  type="button"
+                  className="btn btn-light"
+                  style={{ fontSize: "1.5em", borderRadius: "10px" }}
+                >
                   Register
                 </button>
               </NavLink>
