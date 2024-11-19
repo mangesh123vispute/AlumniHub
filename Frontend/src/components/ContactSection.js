a0ac847c805e0cb03fc7c2605db7aaae5ff1ba05
Rohan Sapkale <99beastrohan@gmail.com>
2024-11-19 14:39:08 +0000

    minor changes

--- Frontend/src/components/ContactSection.js
+++ Frontend/src/components/ContactSection.js
@@ -8,13 +8,13 @@ const ContactSection = () => {
         <div className="flex flex-wrap justify-center lg:justify-between">
           {/* Contact Info */}
           <div className="w-full lg:w-11/12 px-2 md:px-4 mb-8 lg:mb-0 mx-auto">
-            <h3 className="text-[clamp(1.8rem, 3vw, 2.3rem)] font-bold text-gray-700 text-center lg:text-left mb-2">
+            <h3 className="text-[clamp(2.8rem, 3vw, 3.3rem)] font-bold text-gray-700 text-center lg:text-left mb-2">
               Welcome To <span className="text-yellow-500">SSBT's</span> College of Engineering & Technology
             </h3>
-            <p className="text-gray-600 text-[clamp(0.8rem, 1.2vw, 1rem)] text-center lg:text-left border-b-2 border-red-800 mb-6">
+            <p className="text-gray-600 text-[clamp(1rem, 1.5vw, 1.2rem)] text-center lg:text-left border-b-2 border-red-800 mb-6">
               EXCELLENCE IN EDUCATION
             </p>
-            
+
             <div className="flex flex-wrap justify-center lg:justify-start">
               {/* Contact Details */}
               {[
@@ -50,10 +50,14 @@ const ContactSection = () => {
                 },
               ].map((info, index) => (
                 <div key={index} className="w-full sm:w-1/2 lg:w-1/3 px-2 mb-6">
-                  <div className="bg-white p-4 lg:p-6 rounded-lg shadow-md text-center h-full">
-                    <i className={`${info.icon} text-[clamp(1.5rem, 3.5vw, 2.5rem)] text-indigo-600 mb-4`}></i>
-                    <h3 className="text-[clamp(1.2rem, 2.5vw, 1.6rem)] font-semibold text-gray-700">{info.title}</h3>
-                    <p className="text-[clamp(0.75rem, 1vw, 0.9rem)] text-gray-600 mt-2">{info.description}</p>
+                  <div className="bg-white p-4 lg:p-6 rounded-lg shadow-lg text-center h-full">
+                    <i className={`${info.icon} text-[clamp(1.5rem, 3vw, 2.5rem)] text-indigo-600 mb-4`}></i>
+                    <h1 className="text-[clamp(0.8rem, 1.2vw, 1rem)] font-semibold text-gray-700">
+                      {info.title}
+                    </h1>
+                    <h3 className="text-[clamp(0.9rem, 1.8vw, 1.2rem)] text-gray-600 mt-2">
+                      {info.description}
+                    </h3>
                   </div>
                 </div>
               ))}
